04125aabe9d2bd92b5b9498205f3f38d3b7f0dce
Iraponan Marinho <iraponan.aires@outlook.com>
2021-06-27 18:19:59 -0300

    Criando os métodos pra ler e mostrar dados

---
+++ src/main/java/br/eti/inovareti/jogoforca/exceptions/InvalidCharacterException.java
@@ -0,0 +1,8 @@
+package br.eti.inovareti.jogoforca.exceptions;
+
+public class InvalidCharacterException extends Exception {
+
+    public InvalidCharacterException(String message) {
+        super(message);
+    }
+}
