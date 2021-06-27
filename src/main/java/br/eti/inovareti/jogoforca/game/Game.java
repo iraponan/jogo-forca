8e1f46c234ea4298fca0e04974ebf79e064adb65
Iraponan Marinho <iraponan.aires@outlook.com>
2021-06-27 17:14:06 -0300

    Criando a classe Word

--- src/main/java/br/eti/inovareti/jogoforca/game/Game.java
+++ src/main/java/br/eti/inovareti/jogoforca/game/Game.java
@@ -1,7 +1,14 @@
 package br.eti.inovareti.jogoforca.game;
 
+import br.eti.inovareti.jogoforca.core.Word;
+
 public class Game {
     public void start() {
-
+        Word word = new Word("casa");
+        word.hasChar('a');
+        word.hasChar('c');
+        word.hasChar('s');
+        System.out.println(word.discovered());
+        System.out.print(word);
     }
 }
