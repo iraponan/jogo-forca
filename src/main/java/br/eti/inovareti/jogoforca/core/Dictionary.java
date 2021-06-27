e0911284d0e78da3b6707ce8934d076cb79c4900
Iraponan Marinho <iraponan.aires@outlook.com>
2021-06-27 20:59:54 -0300

    Criando múltiplos dicionários

--- src/main/java/br/eti/inovareti/jogoforca/core/Dictionary.java
+++ src/main/java/br/eti/inovareti/jogoforca/core/Dictionary.java
@@ -1,43 +1,17 @@
 package br.eti.inovareti.jogoforca.core;
 
-import br.eti.inovareti.jogoforca.exceptions.GameException;
-import br.eti.inovareti.jogoforca.utils.RandomUtils;
+public abstract class Dictionary {
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-public class Dictionary {
-
-    private static final String FILE_NAME = "dicionario.txt";
     private static Dictionary instance;
-    private List<String> words = new ArrayList<>();
-
-    private Dictionary() {
-        load();
-    }
 
     public static Dictionary getInstance() {
         if (instance == null) {
-            instance = new Dictionary();
+            instance = new FileDictionary();
         }
         return instance;
     }
 
-    private void load() {
-        try (Scanner scanner = new Scanner(getClass().getResourceAsStream("/" + FILE_NAME))) {
-            while (scanner.hasNextLine()) {
-                String word = scanner.nextLine().trim();
-                words.add(word);
-            }
-            if (words.size() == 0) {
-                throw new GameException("A lista de palavras não pode ser vazia!");
-            }
-        }
-    }
+    public abstract Word nextWord();
 
-    public Word nextWord() {
-        int pos = RandomUtils.newRandomNumber(0, words.size());
-        return new Word(words.get(pos));
-    }
+    public abstract String getName();
 }
