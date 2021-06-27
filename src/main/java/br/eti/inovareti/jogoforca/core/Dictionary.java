a962de765a93b55115cc17838810bb4d6ca17ed7
Iraponan Marinho <iraponan.aires@outlook.com>
2021-06-27 17:46:21 -0300

    Criando a classe Dictionary

--- src/main/java/br/eti/inovareti/jogoforca/core/Dictionary.java
+++ src/main/java/br/eti/inovareti/jogoforca/core/Dictionary.java
@@ -1,4 +1,31 @@
 package br.eti.inovareti.jogoforca.core;
 
+import br.eti.inovareti.jogoforca.exceptions.GameException;
+import br.eti.inovareti.jogoforca.utils.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Dictionary {
+
+    private static final String FILE_NAME = "dicionario.txt";
+    private List<String> words = new ArrayList<>();
+
+    private void load() {
+        try (Scanner scanner = new Scanner(getClass().getResourceAsStream("/" + FILE_NAME))) {
+            while (scanner.hasNextLine()) {
+                String word = scanner.nextLine().trim();
+                words.add(word);
+            }
+            if (words.size() == 0) {
+                throw new GameException("A lista de palavras não pode ser vazia!");
+            }
+        }
+    }
+
+    public Word nextWord() {
+        int pos = RandomUtils.newRandomNumber(0, words.size());
+        return new Word(words.get(pos));
+    }
 }
---
+++ src/main/java/br/eti/inovareti/jogoforca/exceptions/GameException.java
@@ -0,0 +1,8 @@
+package br.eti.inovareti.jogoforca.exceptions;
+
+public class GameException extends RuntimeException{
+
+    public GameException(String message) {
+        super(message);
+    }
+}
