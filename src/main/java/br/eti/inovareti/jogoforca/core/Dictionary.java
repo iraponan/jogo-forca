989e85db822df6d081b2b2dcefe562a8bfe8daf5
Iraponan Marinho <iraponan.aires@outlook.com>
2021-06-27 21:14:49 -0300

    Escolhendo o dicionário a ser usado

--- src/main/java/br/eti/inovareti/jogoforca/core/Dictionary.java
+++ src/main/java/br/eti/inovareti/jogoforca/core/Dictionary.java
@@ -1,12 +1,21 @@
 package br.eti.inovareti.jogoforca.core;
 
+import java.lang.reflect.Constructor;
+
 public abstract class Dictionary {
 
     private static Dictionary instance;
 
     public static Dictionary getInstance() {
         if (instance == null) {
-            instance = new FileDictionary();
+            try {
+                String dictionaryClassName = Config.get("dictionaryClassName");
+                Class<?> aClass = Class.forName(dictionaryClassName);
+                Constructor<?> constructor = aClass.getConstructor();
+                instance = (Dictionary) constructor.newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
         return instance;
     }
